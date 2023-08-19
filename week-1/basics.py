d2827ba217f3062039c56bbf54e25b06865ddcaa
marksikaundi <emmanuelmark117@gmail.com>
2023-08-19 08:39:00 +0200

    solved excerse for python

--- week-1/basics.py
+++ week-1/basics.py
@@ -31,3 +31,19 @@ print(mystring)
 mystring = "Dont worry about aposstrophes"
 print(mystring)
 
+# Exercise on variables
+# The target of this exercise is to create a string, an integer, and a floating point number. The string should be named mystring and should contain the word "hello". The floating point number should be named myfloat and should contain the number 10.0, and the integer should be named myint and should contain the number 20.
+mystring = "hello"
+myfloat = 10.0
+myint = 20
+
+# Testing codes
+if mystring == "hello":
+    print("string: %s" % mystring)
+if isinstance(myfloat, float) and myfloat == 10.0:
+    print("Float: %f" % myfloat)
+if isinstance(myint, int) and myint == 20:
+    print("Integer: %d" % myint)
+
+
+
