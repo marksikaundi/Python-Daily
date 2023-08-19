b96dcc5ad85deec11ec4a844d707fffa4136528f
marksikaundi <emmanuelmark117@gmail.com>
2023-08-19 08:29:30 +0200

    worked on int, strings, float and few examples

--- week-1/basics.py
+++ week-1/basics.py
@@ -9,3 +9,25 @@ myfloat = 7.0
 print(myfloat)
 myfloat = float(7)
 print(myfloat)
+
+# Numbers in python
+# when you want to define an integer, use the following syntax
+myint = 23
+print(myint)
+
+# when you working with fload numbers, you may use the following syntax
+myfloat = 7.0
+print(myfloat)
+myfloat = float(7)
+print(myfloat)
+
+# working with strings, you can define either with single quotes or double quotes
+mystring = 'hello world'
+print(mystring)
+mystring = "Hello World"
+print(mystring)
+
+# The difference between the two is that using double quotes makes it easy to include apostrophes (whereas these would terminate the string if using single quotes)
+mystring = "Dont worry about aposstrophes"
+print(mystring)
+
