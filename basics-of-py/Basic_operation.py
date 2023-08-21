7c1c319797f3b84670e17865b82b3f41a56c8631
marksikaundi <emmanuelmark117@gmail.com>
2023-08-21 14:27:24 +0200

    completed examples of basics operations in py

---
+++ basics-of-py/Basic_operation.py
@@ -0,0 +1,45 @@
+# This section explains how to use basic operators in Python.
+
+# Arithmetic Operators
+# Just as any other programming languages, the addition, subtraction, multiplication, and division operators can be used with numbers.
+
+number = 1 + 2 * 3 / 4.0
+print(number)
+
+# modulo (%)
+remainder = 11 % 3
+print(remainder)
+
+# Using two multiplication symbols makes a power relationship.
+squared = 7 ** 2
+cube = 2 ** 3
+print(squared)
+print(cube)
+
+# Using Operators with Strings
+# Python supports concatenating strings using the addition operator:
+
+helloworld = "hello" + "" + "world"
+print(helloworld)
+
+# Python also supports multiplying strings to form a string with a repeating sequence:
+programmersmemes = "Mark" * 10
+print(programmersmemes)
+
+# Using Operators with Lists
+# Lists can be joined with the addition operators:
+even_numbers = [2,4,6,8]
+add_numbers = [1,3,5,7]
+all_numbers = add_numbers + even_numbers
+print(all_numbers)
+
+# Just as in strings, Python supports forming new lists with a repeating sequence using the multiplication operator:
+
+# Just as in strings, Python supports forming new lists with a repeating sequence using the multiplication operator:
+print([1,2,3] * 3)
+
+
+
+
+
+
