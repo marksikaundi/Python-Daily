b0161e3a2b6f1ecaf0d1024d0fff9eb35f8b99a2
marksikaundi <emmanuelmark117@gmail.com>
2023-08-21 14:32:42 +0200

    added solutions to the exercise

--- basics-of-py/Basic_operation.py
+++ basics-of-py/Basic_operation.py
@@ -38,8 +38,23 @@ print(all_numbers)
 # Just as in strings, Python supports forming new lists with a repeating sequence using the multiplication operator:
 print([1,2,3] * 3)
 
+# EXERCISE DESCRIPTION
+# The target of this exercise is to create two lists called x_list and y_list, which contain 10 instances of the variables x and y, respectively. You are also required to create a list called big_list, which contains the variables x and y, 10 times each, by concatenating the two lists you have created.
+x = object()
+y = object()
 
+# SOLUTIONS: updated the codes here
+x_list = [x] * 10
+y_list = [y] * 10
+big_list = x_list + y_list
 
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
 
-
+# testing code
+if x_list.count(x) == 10 and y_list.count(y) == 10:
+    print("Almost there...")
+if big_list.count(x) == 10 and big_list.count(y) == 10:
+    print("Great!")
 
