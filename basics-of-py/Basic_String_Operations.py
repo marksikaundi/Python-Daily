8a4de2c312023b3af95f24b62805931a8209e7fb
marksikaundi <emmanuelmark117@gmail.com>
2023-08-22 10:02:59 +0200

    created small

---
+++ basics-of-py/Basic_String_Operations.py
@@ -0,0 +1,17 @@
+# Basic String Operations
+# Strings are bits of text. They can be defined as anything between quotes:
+
+astring = "Hello world"
+astring2 = 'Hello world!'
+print(astring)
+print(astring2)
+# As you can see, the first thing you learned was printing a simple sentence. This sentence was stored by Python as a string. However, instead of immediately printing strings out, we will explore the various things you can do to them. You can also use single quotes to assign a string. However, you will face problems if the value to be assigned itself contains single quotes. For example to assign the string in these bracket(single quotes are ' ') you need to use double quotes only like this
+
+astring = "Hello world!"
+print("singer quotes are ''")
+
+print(len(astring))
+
+
+
+
