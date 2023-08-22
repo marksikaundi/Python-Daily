26674f6722235da74c2e391b57763d619368b2fa
marksikaundi <emmanuelmark117@gmail.com>
2023-08-22 11:43:01 +0200

    solved exercise about conditions

--- basics-of-py/Conditions.py
+++ basics-of-py/Conditions.py
@@ -42,3 +42,44 @@ if x == 2:
     print("x equals two!")
 else:
     print("x is not equal to 2.")
+    
+# A statement is evaulated as true if one of the following is correct: 1. The "True" boolean variable is given, or calculated using an expression, such as an arithmetic comparison. 2. An object which is not considered "empty" is passed.
+
+# Here are some examples for objects which are considered as empty: 1. An empty string: "" 2. An empty list: [] 3. The number zero: 0 4. The false boolean variable: False
+
+# The 'is' operator
+# Unlike the double equals operator "==", the "is" operator does not match the values of the variables, but the instances themselves. For example:
+x = [1,2,3]
+y = [1,2,3]
+print(x == y) #prints out True
+print(x is y) #Prints out False
+
+# The "not" operator
+# Using "not" before a boolean expression inverts it
+print(not False) #Prints out True
+print((not False) == (False)) #Prints out False
+
+# EXERCISE SOLUTION ABOUT CONDITION
+# change this code
+number = 16
+second_number = 0
+first_array = [1,2,3]
+second_array = [1,2]
+
+if number > 15:
+    print("1")
+
+if first_array:
+    print("2")
+
+if len(second_array) == 2:
+    print("3")
+
+if len(first_array) + len(second_array) == 5:
+    print("4")
+
+if first_array and first_array[0] == 1:
+    print("5")
+
+if not second_number:
+    print("6")
--- basics-of-py/Questions.py
+++ basics-of-py/Questions.py
@@ -67,6 +67,31 @@ if s.endswith("ome!"):
 # each containing only a word
 print("Split the words of the string: %s" % s.split(" "))
 
+# Exercise
+# Change the variables in the first section, so that each if statement resolves as True.
+# change this code
+number = 10
+second_number = 10
+first_array = []
+second_array = [1,2,3]
+
+if number > 15:
+    print("1")
+
+if first_array:
+    print("2")
+
+if len(second_array) == 2:
+    print("3")
+
+if len(first_array) + len(second_array) == 5:
+    print("4")
+
+if first_array and first_array[0] == 1:
+    print("5")
+
+if not second_number:
+    print("6")
 
 
 
