44592bf56ded3054e2bcc4b47b230ec974538a0e
marksikaundi <emmanuelmark117@gmail.com>
2023-08-27 17:22:17 +0200

    completed functions

--- basics-of-py/Function.py
+++ basics-of-py/Function.py
@@ -42,7 +42,21 @@ my_function_with_args("John Doe", "a great year!")
 # after this line x will hold the value 3!
 x = sum_two_numbers(1,2)
 
+# Solutions to function - (EXERCISE)
+# Modify this function to return a list of strings as defined above
+def list_benefits():
+    return "More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"
+
+# Modify this function to concatenate to each benefit - " is a benefit of functions!"
+def build_sentence(benefit):
+    return "%s is a benefit of functions!" % benefit
 
 
+def name_the_benefits_of_functions():
+    list_of_benefits = list_benefits()
+    for benefit in list_of_benefits:
+        print(build_sentence(benefit))
+
+name_the_benefits_of_functions()
 
 
