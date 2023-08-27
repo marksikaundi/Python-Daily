892a2626e60f7f68a9e72279d97b763e279c7fbc
marksikaundi <emmanuelmark117@gmail.com>
2023-08-27 17:17:51 +0200

    completed examples for func

--- basics-of-py/Function.py
+++ basics-of-py/Function.py
@@ -21,6 +21,26 @@ def my_function_with_args(username, greeting):
 def sum_two_numbers(a, b):
     return a + b
 
+# How do you call functions in Python?
+# Simply write the function's name followed by (), placing any required arguments within the brackets. For example, lets call the functions written above (in the previous example):
+# Define our 3 functions
+def my_function():
+    print("Hello From My Function!")
+
+def my_function_with_args(username, greeting):
+    print("Hello, %s, From My Function!, I wish you %s"%(username, greeting))
+
+def sum_two_numbers(a, b):
+    return a + b
+
+# print(a simple greeting)
+my_function()
+
+#prints - "Hello, John Doe, From My Function!, I wish you a great year!"
+my_function_with_args("John Doe", "a great year!")
+
+# after this line x will hold the value 3!
+x = sum_two_numbers(1,2)
 
 
 
